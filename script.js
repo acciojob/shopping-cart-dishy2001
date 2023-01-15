ee800d3164795993f8f68690c3e042e69171d408
Deekshya Karki <99348458+dishy2001@users.noreply.github.com>
2023-01-15 20:33:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let grandtotal = 0;
 const addData = () => {
   let name = document.getElementById('item-name-input').value;
-  let qty = document.getElementById('item-qty-input').value;
+  // let qty = document.getElementById('item-qty-input').value;
   let price = document.getElementById('item-price-input').value;
   let table = document.getElementById('table1');
   let newRow = table.insertRow(table.rows.length - 1);
   let namecell = newRow.insertCell(0);
-  let qtycell = newRow.insertCell(1);
-  let pricecell = newRow.insertCell(2);
-  let totalprice = newRow.insertCell(3);
-  if (qty <= 0 && price <= 0) {
-	  document.getElementById('item-qty-input').value = "";
-	  document.getElementById('item-price-input').value = "";
-	  return;
-  }
-  if (qty <= 0) {
-	  document.getElementById('item-qty-input').value = "";
-	  return;
-  }
+  // let qtycell = newRow.insertCell(1);
+  let pricecell = newRow.insertCell(1);
+  let totalprice = newRow.insertCell(2);
+  // if (qty <= 0 && price <= 0) {
+	 //  document.getElementById('item-qty-input').value = "";
+	 //  document.getElementById('item-price-input').value = "";
+	 //  return;
+  // }
+  // if (qty <= 0) {
+	 //  document.getElementById('item-qty-input').value = "";
+	 //  return;
+  // }
   if (price <= 0) {
 	  document.getElementById('item-price-input').value = "";
 	  return;
@@ -25,10 +25,11 @@ const addData = () => {
   namecell.classList.add("item");
   totalprice.classList.add("price");
   namecell.innerHTML = name;
-  qtycell.innerHTML = qty;
+  // qtycell.innerHTML = qty;
   pricecell.innerHTML = price;
-  totalprice.innerHTML = qty * price;
-  grandtotal += qty * price;
+  // totalprice.innerHTML = qty * price;
+  grandtotal += price;
   document.getElementById('total').innerHTML = "Total Ammount " + grandtotal + " $";
-  document.getElementById('myinputs').reset();
+  document.getElementById('item-name-input').value="";
+  document.getElementById('item-price-input').value="";
 }
